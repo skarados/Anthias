4cd2f4e3922a894eb1e2a5c02ad8251301769750
skarados <55186916+skarados@users.noreply.github.com>
2025-01-30 16:04:32 +0100

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -7,11 +7,11 @@ set -euo pipefail
 
 BRANCH="master"
 ANSIBLE_PLAYBOOK_ARGS=()
-REPOSITORY="https://github.com/Screenly/Anthias.git"
+REPOSITORY="https://github.com/skarados/Anthias.git"
 ANTHIAS_REPO_DIR="/home/${USER}/screenly"
-GITHUB_API_REPO_URL="https://api.github.com/repos/Screenly/Anthias"
-GITHUB_RELEASES_URL="https://github.com/Screenly/Anthias/releases"
-GITHUB_RAW_URL="https://raw.githubusercontent.com/Screenly/Anthias"
+GITHUB_API_REPO_URL="https://api.github.com/repos/skarados/Anthias"
+GITHUB_RELEASES_URL="https://github.com/skarados/Anthias/releases"
+GITHUB_RAW_URL="https://raw.githubusercontent.com/skarados/Anthias"
 DOCKER_TAG="latest"
 UPGRADE_SCRIPT_PATH="${ANTHIAS_REPO_DIR}/bin/upgrade_containers.sh"
 ARCHITECTURE=$(uname -m)
